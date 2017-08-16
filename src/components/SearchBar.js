06a5b48f9445d3a7c4ef936b2f815c52470199e6
Sharon <sharon@Sharons-MacBook-Pro.local>
2017-08-16 13:47:14 +1000

    Added predefied search term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import {Typeahead} from 'react-bootstrap-typeahead';
+
 import ListBooks from "./ListBooks";
 
 /**
@@ -10,15 +12,17 @@ import ListBooks from "./ListBooks";
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { term: "" };
+    this.onInputChange=this.onInputChange.bind(this)
+    this.state = { term: ['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'History', 'History', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Program Javascript', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'] };
   }
 
   onInputChange(term) {
+    console.log('term', term);
     this.setState({ term });
     //BooksAPI returns 403 if entered a search string then using backspace to
     //clean up the input string
     if (term.length > 0) {
-      this.props.onSearchTermChange(term);
+      this.props.onSearchTermChange(term[0]);
    }
   }
 
@@ -36,17 +40,23 @@ class SearchBar extends Component {
     const { books } = this.props;
 
     return (
-      <div className="search-books">
-        <div className="search-books-bar">
-           <Link className="close-search" to="/"  onClick={this.cleanSeachResult}>Back</Link>
-           <div className="search-books-input-wrapper">
+      <div>
+        <div className="input-group">
+          <span className="input-group-addon" id="sizing-addon2">
+           <Link to="/"  onClick={this.cleanSeachResult}>Back</Link></span>
+             <Typeahead
+              onChange={this.onInputChange}
+              options={this.state.term}
+              />
+        </div>
+      
+           {/*<div className="search-books-input-wrapper">
              <input
                value={this.state.term}
                placeholder='Search by title or author'
                onChange={event => this.onInputChange(event.target.value)}
-             />
-           </div>
-        </div>
+             />*/}
+  
         <div className="search-books-results">
             <ol className="books-grid">
             {books !== undefined && books.length > 0 &&
